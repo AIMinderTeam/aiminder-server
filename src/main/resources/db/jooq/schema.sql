6dc598bf44e11061d9963eb77d70627b19c0ebca
Sangmin Lee <nalsm98@naver.com>
2025-10-12 20:32:25 +0900

    feat: 알림 테이블 생성 및 스키마 수정
    - `notifications` 테이블 추가로 알림 데이터 관리 기능 확장:
      - `receiver_id` 기반 외래 키, 유형, 제목, 설명, 메타데이터, 확인 여부 필드 포함
      - 외래 키 제약 조건과 복합 인덱스로 데이터 무결성 및 조회 성능 보장
    - 기존 `schema.sql`에 알림 테이블 정의 추가
    - 스키마 정비로 데이터 관리 확장성 및 일관성 강화

--- src/main/resources/db/jooq/schema.sql
+++ src/main/resources/db/jooq/schema.sql
@@ -125,3 +125,17 @@ CREATE TABLE chat
   created_at      TIMESTAMP             NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS notifications
+(
+  notification_id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+  type            VARCHAR(30)      NOT NULL,
+  title           VARCHAR(255)     NOT NULL,
+  description     TEXT             NOT NULL,
+  metadata        JSONB            NOT NULL,
+  checked         BOOLEAN          NOT NULL,
+  receiver_id     uuid             NOT NULL
+    CONSTRAINT fk_notifications_receiver_id REFERENCES users (user_id) ON DELETE CASCADE,
+  created_at      TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at      TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at      TIMESTAMP
+);
